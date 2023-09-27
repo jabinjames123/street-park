bbd1fb052f68285004b6ec4167da1c79644b8e98
jabinjames123 <146153489+jabinjames123@users.noreply.github.com>
2023-09-27 19:14:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var getstarted=document.getElementsByClassName("req")[0];
+getstarted.addEventListener("click",function(event){
+    event.preventDefault();
+    window.location.href="newpage.html";
+    console.log("hhaii")
+});
+// getstarted.addEventListener("click",function(event){
+   
+// });
---
+++ newpage.js
@@ -0,0 +1,18 @@
+var getstarted=document.getElementsByClassName("booknow")[0];
+getstarted.addEventListener("click",function(event){
+    event.preventDefault();
+    window.location.href="main.html";
+    console.log("hhaii")
+});
+var getstarted=document.getElementsByClassName("booknow1")[0];
+getstarted.addEventListener("click",function(event){
+    event.preventDefault();
+    window.location.href="main.html";
+    console.log("hhaii")
+});
+var getstarted=document.getElementsByClassName("booknow2")[0];
+getstarted.addEventListener("click",function(event){
+    event.preventDefault();
+    window.location.href="main.html";
+    console.log("hhaii")
+});
